e5c0836ac44fd2c3ec225bbaa92cfa1b26552586
Rishabh Gupta <3rishabh3@gmail.com>
2022-02-24 15:05:07 +0530

    Front end connected to smart contract

--- src/App.js
+++ src/App.js
@@ -1,25 +1,130 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+import Web3 from "web3";
+import Crowdfundingabi from "./contracts/Crowdfunding.json";
+
+const App = () => {
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [crowdfundingSm, setCrowdfundingSm] = useState(null);
+
+  useEffect(() => {
+    loadWeb3();
+    loadBlockchainData();
+  }, []);
+
+  const loadWeb3 = async () => {
+    if (window.ethereum) {
+      await window.ethereum.enable();
+    } else {
+      alert("Please connect to metamask");
+    }
+  };
+
+  const loadBlockchainData = async () => {
+    if (typeof window.ethereum == "undefined") {
+      return;
+    }
+
+    const web3 = new Web3(window.ethereum);
+    const accounts = await web3.eth.getAccounts();
+
+    setCurrentAccount(accounts[0]);
+    const networkId = await web3.eth.net.getId();
+
+    const networkData = Crowdfundingabi.networks[networkId];
+
+    if (networkData) {
+      const crowdfunding = new web3.eth.Contract(
+        Crowdfundingabi.abi,
+        networkData.address
+      );
+      console.log("Smart contract address:", networkData.address);
+      console.log("Balance:", await web3.eth.getBalance(networkData.address));
+      console.log("Requests:", await crowdfunding.methods.requests(3).call());
+      setCrowdfundingSm(crowdfunding);
+    }
+  };
+
+  const createRequest = async (
+    category,
+    description,
+    recipientAddress,
+    timeLimit,
+    target
+  ) => {
+    try {
+      await crowdfundingSm.methods
+        .createRequests(
+          category,
+          description,
+          recipientAddress,
+          timeLimit,
+          target
+        )
+        .send({ from: currentAccount });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const sendETH = async (id, amt) => {
+    try {
+      await crowdfundingSm.methods
+        .sendEth(id)
+        .send({ from: currentAccount, value: amt });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const makePayment = async (id) => {
+    try {
+      console.log(
+        await crowdfundingSm.methods
+          .makePayment(id)
+          .send({ from: currentAccount })
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="container text-center">
+        <h1 className="mt-3 text-center">{currentAccount}</h1>
+        <button
+          onClick={() => {
+            createRequest(
+              "sports",
+              "cricket",
+              "0x024cd4b48E97ddb9B2C6aD3444B9181183Ac0d07",
+              1000000,
+              Web3.utils.toWei("1", "ether")
+            );
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          create request
+        </button>
+
+        <button
+          onClick={() => {
+            sendETH(3, Web3.utils.toWei("1", "ether"));
+          }}
+        >
+          send eth
+        </button>
+
+        <button
+          onClick={() => {
+            makePayment(3);
+          }}
+        >
+          make Payment
+        </button>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
